fca41e402b6212508d06f6e136b99a53e47c26c9
MikhailV89 <114851579+MikhailV89@users.noreply.github.com>
2023-08-07 20:11:01 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,117 @@
+let userData = {
+    'USD': 1000,
+    'EUR': 900,
+    'UAH': 15000,
+    'BIF': 20000,
+    'AOA': 100
+};
+
+let bankData = {
+    'USD': {
+        max: 3000,
+        min: 100,
+        img: '💵'
+    },
+    'EUR': {
+        max: 1000,
+        min: 50,
+        img: '💶'
+    },
+    'UAH': {
+        max: 0,
+        min: 0,
+        img: '💴'
+    },
+    'GBP': {
+        max: 10000,
+        min: 100,
+        img: '💷'
+    }
+};
+
+function getUserInput(message) {
+    return new Promise(resolve => {
+        const userInput = confirm(message);
+        resolve(userInput);
+    });
+}
+
+function getCardBalance(userData) {
+    const availableCurrencies = Object.keys(userData);
+    return new Promise((resolve, reject) => {
+        function requestCurrency() {
+            let currency = prompt(`Введіть назву валюти (${availableCurrencies.join(', ')}):`);
+            if (currency === null) {
+                reject('Ви скасували введення назви валюти.');
+            } else {
+                currency = currency.toUpperCase();
+                if (userData.hasOwnProperty(currency)) {
+                    resolve(currency);
+                } else {
+                    console.log('Введена недопустимая валюта.');
+                    requestCurrency();
+                }
+            }
+        }
+        requestCurrency();
+    });
+}
+
+function getAmount(currency, bankData) {
+    return new Promise((resolve, reject) => {
+        const maxAmount = bankData[currency].max;
+        const minAmount = bankData[currency].min;
+        let amount = parseFloat(prompt(`Введіть суму для зняття (${currency}):`));
+
+        if (isNaN(amount) || amount < minAmount) {
+            reject(`Введена сума менша за допустимий мінімум. Мінімальна сума виведення: ${minAmount}`);
+        } else if (amount > maxAmount) {
+            reject(`Введена сума перевищує дозволений максимум. Максимальна сума виводу: ${maxAmount}`);
+        } else {
+            resolve(amount);
+        }
+    });
+}
+
+function getMoney(userData, bankData) {
+    
+    return getUserInput('Переглянути баланс карти?')
+        .then(checkBalance => {
+            if (checkBalance) {
+                return getCardBalance(userData)
+                    .then(currency => {
+                        console.log(`Баланс: ${userData[currency]} ${currency}`);
+                    })
+                    .catch(err => {
+                        throw err;
+                    });
+            } else {
+                return getCardBalance(userData)
+                    .then(currency => {
+                        if (bankData.hasOwnProperty(currency)) {
+                            return getAmount(currency, bankData)
+                                .then(amount => {
+                                    userData[currency] -= amount;
+                                    console.log(`Ось ваша готівка: ${amount} ${currency} ${bankData[currency].img}`);
+                                })
+                                .catch(err => {
+                                    console.log(err);
+                                });
+                        } else {
+                            console.log('Валюта відсутня у банкоматі.');
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err);
+                    });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        })
+        .finally(() => {
+            console.log('Дякуємо, гарного дня 😊');
+        });
+}
+
+getMoney(userData, bankData);
